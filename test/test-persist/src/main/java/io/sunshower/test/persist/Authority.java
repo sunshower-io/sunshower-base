83cca761d1ead39ceb53085ef4bd74aa6a0a6b27
josiahhaswell <josiah.haswell@gmail.com>
2017-12-22 13:29:37 -0700

    Adding parameter as valid target for annotations

--- test/test-persist/src/main/java/io/sunshower/test/persist/Authority.java
+++ test/test-persist/src/main/java/io/sunshower/test/persist/Authority.java
@@ -6,7 +6,12 @@ import java.lang.annotation.*;
 @Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Target({
+        ElementType.TYPE, 
+        ElementType.METHOD, 
+        ElementType.FIELD, 
+        ElementType.PARAMETER
+})
 public @interface Authority {
     String value();
 }
