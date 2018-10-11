6f0eb1ba5eab60b2e1aae16264ada0ad0e2bb885
Josiah Haswell <josiah.haswell@gmail.com>
2018-10-10 19:05:15 -0600

    Trying settings

--- persist/persist-core/src/test/java/io/sunshower/persist/core/ClasspathFilesProvider.java
+++ persist/persist-core/src/test/java/io/sunshower/persist/core/ClasspathFilesProvider.java
@@ -3,14 +3,18 @@ package io.sunshower.persist.core;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+
+import lombok.SneakyThrows;
 import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
 
 public class ClasspathFilesProvider implements ConfigFilesProvider {
   public ClasspathFilesProvider(String s) {}
 
   @Override
+  @SneakyThrows
   public Iterable<Path> getConfigFiles() {
-    return Collections.singleton(
-        Paths.get(ClassLoader.getSystemResource("application-2.yml").getFile()).toFile().toPath());
+    return Paths.get(ClassLoader.getSystemResource("application-2.yml").toURI());
+//    return Collections.singleton(
+//        Paths.get(ClasspathFilesProvider.class.getResource("application-2.yml").getFile()).toFile().toPath());
   }
 }
