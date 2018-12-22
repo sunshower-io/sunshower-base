0a2ab1f458b3bb18cb5843bc65f1701a220da4f2
Josiah Haswell <josiah.haswell@gmail.com>
2018-12-22 00:11:53 -0700

    Adding ref

--- common/core/src/main/java/io/sunshower/common/OS.java
+++ common/core/src/main/java/io/sunshower/common/OS.java
@@ -3,31 +3,29 @@ package io.sunshower.common;
 import java.util.Locale;
 
 public class OS {
-    public enum Type {
-        Windows,
-        Linux,
-        Apple,
-        Other
+  public enum Type {
+    Windows,
+    Linux,
+    Apple,
+    Other
+  }
+
+  protected static Type type;
+ /// http://www.docjar.com/html/api/org/apache/commons/lang/SystemUtils.java.html
+
+  public static Type resolve() {
+    if (type == null) {
+      String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+      if ((OS.contains("mac")) || (OS.contains("darwin"))) {
+        type = Type.Apple;
+      } else if (OS.contains("win")) {
+        type = Type.Windows;
+      } else if (OS.contains("nux")) {
+        type = Type.Linux;
+      } else {
+        type = Type.Other;
+      }
     }
-
-
-    protected static Type type;
-
-    public static Type resolve() {
-        if (type == null) {
-            String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
-            if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
-                type = Type.Apple
-                ;
-            } else if (OS.indexOf("win") >= 0) {
-                type = Type.Windows;
-            } else if (OS.indexOf("nux") >= 0) {
-                type = Type.Linux;
-            } else {
-                type = Type.Other;
-            }
-        }
-        return type;
-    }
-
+    return type;
+  }
 }
