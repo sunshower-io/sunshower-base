dd994410aa43566e07577ed78549e2643422cf9b
Josiah Haswell <josiah.haswell@gmail.com>
2019-03-07 21:23:59 -0700

    Removing delta

--- common/lang/src/test/java/io/sunshower/lang/primitives/DoublesTest.java
+++ common/lang/src/test/java/io/sunshower/lang/primitives/DoublesTest.java
@@ -14,7 +14,7 @@ public class DoublesTest {
         new double[] {0.0d, 4.0d, 99.45d, 113414123451234.4d, Double.MAX_VALUE, Double.MIN_VALUE};
 
     double[] c = Doubles.fromByteArray(Doubles.toByteArray(d));
-    assertArrayEquals(d, c, Doubles.next(0.0d));
+    assertArrayEquals(d, c);
   }
 
   @Test
