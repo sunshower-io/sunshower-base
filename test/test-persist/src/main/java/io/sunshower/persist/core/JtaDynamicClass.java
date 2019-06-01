e65fff5465f9e1c921c25df7cdb6a429e6b60cc7
Josiah Haswell <josiah.haswell@gmail.com>
2019-05-31 23:26:45 -0600

    Fixing test-persist

--- test/test-persist/src/main/java/io/sunshower/persist/core/JtaDynamicClass.java
+++ test/test-persist/src/main/java/io/sunshower/persist/core/JtaDynamicClass.java
@@ -18,19 +18,6 @@ public class JtaDynamicClass implements DynamicClass {
 
   @Override
   public XADataSource getDataSource(String dbName) throws SQLException {
-    //    if (datasource == null) {
-    //      synchronized (lock) {
-    //        if (datasource == null) {
-    //          final JdbcDataSource dataSource = new JdbcDataSource();
-    //          dataSource.setUser("sa");
-    //          dataSource.setPassword("");
-    //          dataSource.setURL("jdbc:" + dbName);
-    //          return (datasource = dataSource);
-    //        }
-    //      }
-    //    }
-    //    return datasource;
-
     final JdbcDataSource dataSource = new JdbcDataSource();
     dataSource.setUser("sa");
     dataSource.setPassword("");
