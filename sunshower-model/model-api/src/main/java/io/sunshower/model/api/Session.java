83c9342b5f1a4a5651a8d1e761f171291aa97cdf
Josiah Haswell <josiah.haswell@gmail.com>
2022-06-07 19:30:37 -0600

    Fixing session

--- sunshower-model/model-api/src/main/java/io/sunshower/model/api/Session.java
+++ sunshower-model/model-api/src/main/java/io/sunshower/model/api/Session.java
@@ -1,59 +1,15 @@
 package io.sunshower.model.api;
 
-import java.util.Collection;
-import java.util.Collections;
 import lombok.val;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class Session implements Authentication {
+public class Session {
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
+  public User getUser() {
     val authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication != null) {
-      return authentication.getAuthorities();
-    }
-    return Collections.emptyList();
-  }
-
-  @Override
-  public String getCredentials() {
-    val principal = getPrincipal();
-    return principal != null ? principal.getPassword() : null;
-  }
-
-  @Override
-  public UserDetails getDetails() {
-    val user = getPrincipal();
-    if (user != null) {
-      return user.getDetails();
+      return (User) authentication.getDetails();
     }
     return null;
-  }
-
-  @Override
-  public User getPrincipal() {
-    val authentication = SecurityContextHolder.getContext().getAuthentication();
-    if (authentication != null) {
-      return (User) authentication.getPrincipal();
-    }
-    return null;
-  }
-
-  @Override
-  public boolean isAuthenticated() {
-    val principal = getPrincipal();
-    return principal != null;
-  }
-
-  @Override
-  public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {}
-
-  @Override
-  public String getName() {
-    val user = getPrincipal();
-    return user != null ? user.getUsername() : null;
   }
 }
